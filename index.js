798c6ae8ae70ac446443556e1b4206db5569e7a6
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-15 15:31:10 -0700

    nodemon is installed, and environment port is changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app =express();
-const PORT = 3000;
+const PORT = process.env.PORT ||3000;
 
 app.get('/',(req,res)=>{
-   res.send('hello')
+   res.send('hello world')
 })
 
 app.get('/api/animals',(req,res)=>{
@@ -11,5 +11,5 @@ app.get('/api/animals',(req,res)=>{
 })
 
 app.listen(PORT, ()=>{
-  console.log(`the app is listening on port ${PORT}`)
+  console.log(`the app is listening on port ${PORT}....`)
 })
