d79ff0bc61b485532ccb923ddda5aeedd011e0cf
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-15 22:39:56 -0700

    the animals array is added more properties, css display for main page is changed

--- animals.js
+++ animals.js
@@ -1,11 +1,11 @@
 const animals=[
-  {id:1, Btype:'Dog', Stype:"Golden Retriever", age:6, placeFound:'West Park', status:'healthy',imageURL:'https://i.ytimg.com/vi/AzaDI3yjb4A/maxresdefault.jpg'},
-  {id:2, Btype:'Cat', Stype:'persian', age:5 ,placeFound:'Front gate of Northern Avenue', status:'starving and skinny', imageURL:'https://dnl7t01l0fo05.cloudfront.net/wp-content/uploads/2017/04/cat-brian-bullied-qatar-3.jpg'},
-  {id:3, Btype:'Dog', Stype:'Husky', age:12 ,placeFound:'sea shore of North Vancouver', status:'leg injured', imageURL:"https://patch.com/img/cdn/users/1081943/2012/07/raw/c39aa26a07be3fb193a85d16a3c85167.jpg"},
-  {id:4, Btype:'Dog', Stype:'Beagle', age:3 ,placeFound:'downtown', status:'dirty but healthy', imageURL:'https://i.ytimg.com/vi/vLRH0y4qPcc/maxresdefault.jpg'
+  {id:1, Btype:'Dog', Stype:"Golden Retriever", age:6, placeFound:'West Park', status:'healthy',imageURL:'https://i.ytimg.com/vi/AzaDI3yjb4A/maxresdefault.jpg', physical_conditon:'When she was found, she does not seems to have appetite, but she recovered that quickly, now she is healthey', description:'2 feet tall, brown eyes, Friendly toward people, pretty versatile'},
+  {id:2, Btype:'Cat', Stype:'persian', age:5 ,placeFound:'Front gate of Northern Avenue', status:'starving and skinny', imageURL:'https://dnl7t01l0fo05.cloudfront.net/wp-content/uploads/2017/04/cat-brian-bullied-qatar-3.jpg', physical_conditon:'he lost vision for one eye, and had disability running, does not have appitite when found, still recovering from that', description:'long furs, small eyes, long mustache, shows some aggression toward people, prefer to be quiet'},
+  {id:3, Btype:'Dog', Stype:'Husky', age:12 ,placeFound:'sea shore of North Vancouver', status:'leg injured', imageURL:"https://patch.com/img/cdn/users/1081943/2012/07/raw/c39aa26a07be3fb193a85d16a3c85167.jpg", physical_conditon:'When he was found, his stomach was full of parasites, and suffered from gastritis, recovered for now', description:'3 feet tall, blue eyes, pretty versatile, shows anxiety when being alone'},
+  {id:4, Btype:'Dog', Stype:'Beagle', age:3 ,placeFound:'downtown', status:'healthy', imageURL:'https://i.ytimg.com/vi/vLRH0y4qPcc/maxresdefault.jpg',physical_conditon:'lost three teeths, suffering from Kidney disease, slowly recovering', description:"1 feet tall, shows strong aggression toward people, tend to protect foods when being fed"
   },
-  {id:5, Btype:'Cat', Stype:'British Shorthair', age:8 ,placeFound:'Yaletown', status:'anixous but healthy', imageURL:'https://thumbs.dreamstime.com/b/grey-british-cat-yellow-eyes-sits-wooden-floor-homeless-hungry-cat-grey-british-cat-yellow-eyes-sits-175650572.jpg'}
+  {id:5, Btype:'Cat', Stype:'British Shorthair', age:8 ,placeFound:'Yaletown', status:'anixous but healthy', imageURL:'https://thumbs.dreamstime.com/b/grey-british-cat-yellow-eyes-sits-wooden-floor-homeless-hungry-cat-grey-british-cat-yellow-eyes-sits-175650572.jpg', physical_conditon:'generally healthy, sporty and smart', description:"grey hair, small eyes, strong sense of land owenership, prefers to be alone"}
 ]
 
 
-module.exports = animals
+module.exports = animals;
